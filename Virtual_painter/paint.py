6b720a7c4b0d47ab69b84e4d9828dc4cfc804af0
ductai26998 <ductai26998@gmail.com>
2021-07-03 23:53:29 +0700

    Draw red, green, blue

--- Virtual_painter/paint.py
+++ Virtual_painter/paint.py
@@ -1,27 +1,30 @@
 import numpy as np
 import cv2 as cv
 
-def detect_color(img, lower_color, upper_color):
+def detect_color(img, thresholds):
     """
         detect color and return (x,y) coordinate of that point
         params:
             img: image_input
             lower_color: lower threshold of color
-
     """
+    points = []
     # covert img to hsv image
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv_img, lower_color, upper_color)
+    for i in range(len(thresholds)):
+        # Threshold the HSV image to get only blue colors
+        mask = cv.inRange(hsv_img, thresholds[i][0], thresholds[i][1])
 
-    # get position of point
-    x, y = get_position(mask)
-    return (x, y)
+        # get position of point
+        x, y = get_position(mask)
+        point = (x, y, i)
+        points.append((point))
+    return points
 
-def draw(points, color):
+def draw(points, colors):
     for point in points:
-        cv.circle(img, (point[0], point[1]), 10, color, -1)
+        cv.circle(img, (point[0], point[1]), 10, colors[point[2]], -1)
 
 def get_position(img):
     # find all contours in image
@@ -30,9 +33,14 @@ def get_position(img):
     for contour in contours:
         area = cv.contourArea(contour) # return numbers of pixel not 0
         if area > 500:
-            p = cv.arcLength(contour, True) # calculates a contour perimeter is closed
-            approx = cv.approxPolyDP(contour, 0.1 * p, True) # Approximates a polygonal curve(s) with the specified precision
-            x, y , w, h = cv.boundingRect(approx) # Calculates the up-right bounding rectangle of a point set or non-zero pixels of gray-scale image.
+            # calculates a contour perimeter is closed
+            p = cv.arcLength(contour, True)
+
+            # Approximates a polygonal curve(s) with the specified precision
+            approx = cv.approxPolyDP(contour, 0.1 * p, True)
+
+            # Calculates the up-right bounding rectangle of a point set or non-zero pixels of gray-scale image.
+            x, y , w, h = cv.boundingRect(approx)
 
     return x + w // 2, y
 
@@ -47,29 +55,37 @@ cap.set(10,150)
 
 points = []
 
+# define range of red color in HSV
+lower_red = np.array([0, 10, 100])
+upper_red = np.array([10, 255, 255])
+red = [0, 0, 255]
+
+# define range of green color in HSV
+lower_green = np.array([40, 70, 72])
+upper_green = np.array([80, 255, 255])
+green = [0, 255, 0]
+
+# define range of blue color in HSV
+lower_blue = np.array([110, 50, 50])
+upper_blue = np.array([130, 255, 255])
+blue = [255, 0, 0]
+
+thresholds = [[lower_red, upper_red],
+              [lower_green, upper_green],
+              [lower_blue, upper_blue]]
+colors = [red, green, blue]
+
 while True:
     _, img = cap.read()
 
-    # define range of red color in HSV
-    lower_red = np.array([0, 100, 20])
-    upper_red = np.array([10, 255, 255])
-    red = [0, 0, 255]
+    # get points of 1 iterator
+    item_points = detect_color(img, thresholds)
 
-    # define range of green color in HSV
-    lower_green = np.array([40, 70, 72])
-    upper_green = np.array([80, 255, 255])
-    green = [0, 255, 0]
-
-    # define range of blue color in HSV
-    lower_blue = np.array([110, 50, 50])
-    upper_blue = np.array([130, 255, 255])
-    blue = [255, 0, 0]
-
-    point = detect_color(img, lower_green, upper_green)
-
-    points.append(point)
+    if len(item_points) != 0:
+        for new_point in item_points:
+            points.append(new_point)
     if len(points) != 0:
-        draw(points, green)
+        draw(points, colors)
     # Show result
     cv.imshow('img', img)
     if cv.waitKey(1) & 0xFF == ord(' '):
